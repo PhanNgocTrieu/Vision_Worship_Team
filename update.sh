544c07c4b6eb2c77627b347768a08ae79d58044b
trieupn <pntrieu200799@gmail.com>
2023-10-22 08:17:45 +0700

    Update files for serve God

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 echo "Processing of Update Github"
 unameOut="$(uname -s)"
+
 case "${unameOut}" in
     Linux*)     machine=Linux;;
     Darwin*)    machine=Mac;;
@@ -15,8 +16,12 @@ if [ "$machine" == "Mac" ]; then
     find . -name ".DS_Store" -exec rm {} \;
 fi
 
+echo "Pulling from USER:" $USER
 git pull
-git add .
-git commit -m "Update files for serve God"
-git push origin God
+
+if [ "$USER" == "tpn" ]; then
+    git add .
+    git commit -m "Update files for serve God"
+    git push origin God
+fi
 echo "Finished Process of Updated"
